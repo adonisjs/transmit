2dd4fca2fe94ab2c2e0a81aa91742f6cb9185dad
Romain Lanz <romain.lanz+github@pm.me>
2023-07-20 10:36:18 +0200

    fix: export RedisTransport

--- index.ts
+++ index.ts
@@ -11,5 +11,6 @@ import './src/types/extended.js'
 
 export { configure } from './configure.js'
 export { Transmit } from './src/transmit.js'
+export { RedisTransport } from './src/transports/redis_transport.js'
 export { defineConfig } from './src/define_config.js'
 export { stubsRoot } from './stubs/index.js'
