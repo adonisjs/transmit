8ef66c616a4ffc934a47e4a4e517dec1406c0dc0
Romain Lanz <romain.lanz+github@pm.me>
2023-09-25 19:46:10 +0200

    chore: add file header copyright

--- src/define_config.ts
+++ src/define_config.ts
@@ -1,3 +1,12 @@
+/*
+ * @adonisjs/transmit
+ *
+ * (c) AdonisJS
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 import type { TransmitConfig } from './types/main.js'
 
 export function defineConfig<T extends TransmitConfig>(config: T): T {
