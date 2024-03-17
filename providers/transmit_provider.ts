30886ec902ebad0256639ea18d895cac2b689eb1
Estéban <esteban.soubiran@insa-cvl.fr>
2024-03-17 12:58:55 +0100

    fix: add missing `await`

--- providers/transmit_provider.ts
+++ providers/transmit_provider.ts
@@ -38,11 +38,11 @@ export default class TransmitProvider {
       transmit.$createStream(ctx)
     })
 
-    router.post('__transmit/subscribe', (ctx) => {
+    router.post('__transmit/subscribe', async (ctx) => {
       const uid = ctx.request.input('uid')
       const channel = ctx.request.input('channel')
 
-      const success = transmit.$subscribeToChannel(uid, channel, ctx)
+      const success = await transmit.$subscribeToChannel(uid, channel, ctx)
 
       if (!success) {
         return ctx.response.badRequest()
@@ -51,11 +51,11 @@ export default class TransmitProvider {
       return ctx.response.noContent()
     })
 
-    router.post('__transmit/unsubscribe', (ctx) => {
+    router.post('__transmit/unsubscribe', async (ctx) => {
       const uid = ctx.request.input('uid')
       const channel = ctx.request.input('channel')
 
-      const success = transmit.$unsubscribeFromChannel(uid, channel, ctx)
+      const success = await transmit.$unsubscribeFromChannel(uid, channel, ctx)
 
       if (!success) {
         return ctx.response.badRequest()
